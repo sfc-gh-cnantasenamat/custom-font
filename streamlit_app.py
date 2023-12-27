594e25a92be75a82fff3c0f0a642fc92a129b885
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-12-27 23:24:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,24 @@ import streamlit as st
 st.title('🎈 Custom font style')
 
 st.header("Step 1 - Import font")
+st.markdown("We're going to use fonts from the Google API: https://fonts.googleapis.com/css?family=Open+Sans")
+
 st.markdown("""
   <style>
     @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700bold);
   </style>
   """, unsafe_allow_html=True)
 
-st.code("""
-st.markdown('''
-  <style>
-    @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700bold);
-  </style>
-''', unsafe_allow_html=True)
-""")
+with st.expander("See code"):
+  st.code("""
+    st.markdown('''
+      <style>
+        @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700bold);
+      </style>
+    ''', unsafe_allow_html=True)
+  """)
 
 st.header("Step 2 - Use the font")
-
-st.markdown("We're going to use fonts from the Google API: https://fonts.googleapis.com/css?family=Open+Sans")
 
 st.markdown("""
   <h1 style="font-family: 'Open Sans', sans-serif;">
@@ -27,13 +28,14 @@ st.markdown("""
   </h1>
 """, unsafe_allow_html=True)
 
-st.code("""
-st.markdown('''
-  <h1 style="font-family: 'Open Sans', sans-serif;">
-    Open Sans
-  </h1>
-''', unsafe_allow_html=True)
-""")
+with st.expander("See code"):
+  st.code("""
+    st.markdown('''
+      <h1 style="font-family: 'Open Sans', sans-serif;">
+        Open Sans
+      </h1>
+    ''', unsafe_allow_html=True)
+  """)
 
 st.markdown("""
   For comparison, the following is Streamlit's default font style:
@@ -42,10 +44,11 @@ st.markdown("""
   </h1>
 """, unsafe_allow_html=True)
 
-st.code("""
-st.markdown('''
-<h1>
-  Open Sans
-</h1>
-''')
+with st.expander("See code"):
+  st.code("""
+    st.markdown('''
+      <h1>
+        Open Sans
+      </h1>
+    ''')
 """)
