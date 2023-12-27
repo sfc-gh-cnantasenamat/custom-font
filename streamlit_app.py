046a396b0c5e779274565a81ed851c81a1dff6ce
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-12-27 23:15:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,22 @@ import streamlit as st
 
 st.title('🎈 Custom font style')
 
+st.header("Step 1 - Import font")
 st.markdown("""
   <style>
     @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700bold);
   </style>
   """, unsafe_allow_html=True)
 
+st.code("""
+st.markdown('''
+  <style>
+    @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700bold);
+  </style>
+''', unsafe_allow_html=True)
+""")
+
+st.header("Step 2 - Use the font")
 st.markdown("""
   The following is using
   <h1 style="font-family: 'Open Sans', sans-serif;">
