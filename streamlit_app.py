15136702778c88f0d61250b273cf9043fd6c0fd2
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-12-27 23:00:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ st.markdown("""
     @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700bold);
   </style>
   """, unsafe_allow_html=True)
+
+st.markdown("""
+  This app is using the Open Sans font style from the Google API: https://fonts.googleapis.com/css?family=Open+Sans
+""")
